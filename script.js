7e44bd84da59a5dcdc6ba8f058d232dd4785c32c
23239kw-ops <23239kw@hvhs.school.nz>
2026-05-03 23:05:17 +0000

    54

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ const HTML_OUTPUT = document.getElementById("databaseOutput");
 // The ref('/') part tells the operation to write to the base level of the database "/"
 // This means it replaces the whole database with message:Hello World
 /**************************************************************/
-function chudMax(){
-  console.log("Running chudMax()")
-  firebase.database().ref('users/Chud').set(
+function bludMax(){
+  console.log("Running bludMax()")
+  firebase.database().ref('users/blud').set(
     {
-      message: 'Hello World!',
+      message: 'Literally anything else',
       age: '22',
     }
   )
@@ -31,8 +31,8 @@ function foidMax(){
   console.log("Running foidMax()")
   firebase.database().ref('users/Foid').set(
     {
-      message: 'Hello World!',
-      age: '23',
+      message: 'Foid',
+      age: '3',
     }
   )
 }
@@ -43,18 +43,12 @@ function clearMessage() {
 
 function simpleRead() {
     console.log("Reading message");
-    firebase.database().ref('/').child('message').once('value', display, fb_readError);
+    firebase.database().ref('/users/Foid').child('message').once('value', display, fb_readError);
     console.log("Leaving simpleRead")
 }
-
 function display(snapshot) {
-    var dbData = snapshot.val();
-    if (dbData == null) {
-        console.log('There was no record when trying to read the message');
-    }
-    else {
-        console.log("The message is: " + dbData)
-    }
+    console.log("Running display(), the message is: " + snapshot.val());
+    HTML_OUTPUT.innerHTML = snapshot.val();
 }
 
   function fb_readError(error) {
